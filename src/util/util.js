ab4d68bdb1d7c2c2f6a4366e55c6f2fda73bc54c
Nick_Cheung <nkkcheung@gmail.com>
2022-10-30 16:11:22 -0700

    resize image not working

--- src/util/util.js
+++ src/util/util.js
@@ -9,6 +9,25 @@ export const getBase64 = async (file, setState) => {
         setState(reader.result);
     };
     reader.onerror = (error) => {
-        console.log('Error: ', error);
+        console.log('1Error: ', error);
+        
     };
+}
+
+export const resizeImage = (file, resizeFactor, quality) => { //quality between 0-1
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+
+    const img = document.createElement('img')
+    img.src = file;
+
+    const curWidth = img.width;
+    const curHeight = img.height;
+
+    context.drawImage(img, 0, 0, curWidth * resizeFactor, curHeight * resizeFactor);
+
+    return canvas.toBlob(blob => {
+        if (blob) img.src = URL.createObjectURL();
+    }, 'image/jpeg', quality)
+
 }
